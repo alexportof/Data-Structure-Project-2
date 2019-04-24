a113602b676bbe071fa6505b45abc96da5b0117e
Álex Porto <alexportoferreira@hotmail.com>
2019-04-24 20:43:22 -0300

    Improviments in tratar_arquivoXX

--- projeto2.c
+++ projeto2.c
@@ -2,43 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-void embaralhar_array(int array[], int n)
-{
-    srand(time(NULL));
-    for (int i = n - 1; i > 0; i--)
-    {
-        int j = rand() % (i + 1);
-        troca(&array[i], &array[j]);
-    }
-}
-void troca(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-int **tratar_arquivo(char name[], int i)
-{
-    FILE *fp;
-    char file[100];
-    sprintf(file, "DataSet/%s/%s_%.2d.txt", name, name, i);
-    printf("%.2d\t", i);
-    printf("abriu\n");
-    fp = fopen(file, "r");
-    if (fp == NULL)
-    {
-        printf("Cannot open file. %d\n", i);
-        exit(2);
-    }
-    // fscanf(fp, "%d", matriz[0][0]);
-
-    fclose(fp);
-    // return matriz;
-}
+void embaralhar_array(int array[], int n);
+void troca(int *a, int *b);
+int **tratar_arquivo(char name[], int i);
 
 int main()
 {
-    int **num;
+    int **matriz_imagem;
     char *teste[30];
     int array_teste[25], array_treino[25], array_geral[50];
     //int **matriz;
@@ -62,10 +32,85 @@ int main()
             i++;
         }
     }
-    for (int i = 0; i < 25; i++)
+    matriz_imagem = tratar_arquivo("asphalt", 1);
+    //printf("%d\n", matriz_imagem[0][0]);
+    //
+    // for (int i = 0; i < 25; i++)
+    // {
+    //     matriz_imagem = tratar_arquivo("asphalt", array_treino[i]);
+    //     //printf("%d", matriz_imagem[0][0]);
+    //     // free(matriz_imagem);
+    // }
+
+    return 0;
+}
+
+void embaralhar_array(int array[], int n)
+{
+    srand(time(NULL));
+    for (int i = n - 1; i > 0; i--)
     {
-        num = tratar_arquivo("asphalt", array_treino[i]);
+        int j = rand() % (i + 1);
+        troca(&array[i], &array[j]);
     }
+}
+void troca(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int **tratar_arquivo(char name[], int arquivo)
+{
+    FILE *fp;
+    char file[100];
+    int **matriz_imagem;
+    int linha = 0, coluna = 0, valor, caractere;
+
+    matriz_imagem = (int **)malloc(sizeof(int *)); //alocação linha
+    matriz_imagem[0] = (int *)malloc(sizeof(int)); // alocação coluna
+    //colunas sempre serao alocadas e linhas realocadas
+
+    if (matriz_imagem[0] == NULL || matriz_imagem == NULL)
+    {
+        printf("deu pra alocar nao");
+        exit(1);
+    }
+    sprintf(file, "DataSet/%s/%s_%.2d.txt", name, name, arquivo);
+    fp = fopen(file, "r");
+    if (fp == NULL)
+    {
+        printf("Não foi possivel abrir o Aquivo %d\n", arquivo);
+        exit(2);
+    }
+    linha = 0;
+    coluna = 0;
+    int a = 0;
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d%c", &valor, &caractere);
+        // printf("%d%c", valor, caractere);
+        matriz_imagem[linha][coluna] = valor;
+
+        if (caractere == '\n')
+        {
+            printf("nunca entra nesse caralho é");
+            a++;
+            linha++;
+            matriz_imagem = (int **)realloc(matriz_imagem, (linha + 1) * sizeof(int *)); //realocando na proxima linha
+            matriz_imagem[linha] = (int *)malloc(sizeof(int));                           //alocando a coluna da proxima linha
+            coluna = 0;
+        }
+        else
+        {
+            printf("%d", a);
+            a++;
+            //coluna++;
+            // matriz_imagem[linha] = (int *)malloc(sizeof(int)); //alocando a coluna da mesma linha
+        }
+    }
+
+    fclose(fp);
 
     return 0;
 }
